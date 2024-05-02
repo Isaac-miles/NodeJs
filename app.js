c741fba881d81d45ab1d02ac8b655f24369c2bcc
isaac-miles <isaacnice247@gmail.com>
2024-05-02 13:03:15 +0100

    set up a server form

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const {log} = console;
 const PORT = 5050;
 
 const server = http.createServer((req,res)=>{
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html')
+        res.write('<header><title>Form Submit</title></header>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="miles><button type="submit">send</button></input></form></body>')
+        res.write('</html')
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html')
     res.write('<body><h1>Hello Miles </h1></body>')
     res.write('</html')
     res.end()
-    
+
     // process.exit()
 })
 
