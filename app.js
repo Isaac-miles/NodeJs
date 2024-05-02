c7348c2a942e70560d774ff92dc15d44a774cf0a
isaac-miles <isaacnice247@gmail.com>
2024-05-02 19:50:06 +0100

    .

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ const server = http.createServer((req,res)=>{
         res.write('<html')
         res.write('<header><title>Form Submit</title></header>')
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">send</button></form></body>')
-      
-
         res.write('</html')
         return res.end();
     }
@@ -22,17 +20,16 @@ const server = http.createServer((req,res)=>{
         const body = [];
         req.on('data',(chunk)=>{
             body.push(chunk);
-
         })
-        req.on('end',()=>{
-            const parsedBody = Buffer.concat(body).toString();
-           const message = parsedBody.split('=')[1].replace('+',' ')
-          fs.writeFileSync('message.txt',message);
-
+       return req.on('end',()=>{
+          const parsedBody = Buffer.concat(body).toString();
+          const message = parsedBody.split('=')[1]
+          fs.writeFile('message.txt',message,(err)=>{
+            res.statusCode = 302;
+            res.setHeader('Location','/')
+            return res.end()
+          });
         })
-        res.statusCode = 302;
-        res.setHeader('Location','/')
-        return res.end()
     }
 
     res.setHeader('Content-Type', 'text/html');
