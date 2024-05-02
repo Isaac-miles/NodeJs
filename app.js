0537f3cc16c79d2c392e12e8cf7af6472344628f
isaac-miles <isaacnice247@gmail.com>
2024-05-02 11:16:53 +0100

    .

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const {log} = console;
+const PORT = 5050;
+
+const server = http.createServer((req,res)=>{
+    res.send("hello")
+})
+
+server.listen(PORT,()=>{
+    log(`server running on port ${PORT}`)
+})
