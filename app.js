4249cdb2bdbf6bcfe39bdb9fff98486fff6ce395
isaac-miles <isaacnice247@gmail.com>
2024-05-02 13:17:27 +0100

    node

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const {log} = console;
 const PORT = 5050;
 
 const server = http.createServer((req,res)=>{
     const url = req.url;
+    const method = req.method;
+
     if(url === '/'){
         res.write('<html')
         res.write('<header><title>Form Submit</title></header>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="miles><button type="submit">send</button></input></form></body>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">send</button></form></body>')
+      
+
         res.write('</html')
+        return res.end();
+    }
+    if(url == '/message' && method ==='POST'){
+        fs.writeFileSync('message.txt', 'hello messages');
+        res.statusCode = 302;
+        res.setHeader('Location','/')
+        return res.end()
     }
 
     res.setHeader('Content-Type', 'text/html');
