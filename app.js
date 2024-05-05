4f03de660ce735e37d1c522ac1ced51c1704b985
isaac-miles <isaacnice247@gmail.com>
2024-05-05 20:01:33 +0100

    .

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 const {log} = console;
 const PORT = 4000;
 
-const server = http.createServer(routes.requestHandler)
+const server = http.createServer(routes.taskHandler)
 
 server.listen(PORT,()=>{
     log(`server running on port ${PORT}`)
