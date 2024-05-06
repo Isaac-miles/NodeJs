d21414c420dc9e94ce3baed65ed0d30f436bf17b
isaac-miles <isaacnice247@gmail.com>
2024-05-06 11:28:45 +0100

    .

--- routes.js
+++ routes.js
@@ -6,10 +6,10 @@ const reqHandler = (req, res)=>{
     const method = req.method;
 
     if(url === '/'){
-        res.write('<html')
-        res.write('<header><title>Form Submit</title></header>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">send</button></form></body>')
-        res.write('</html')
+        res.write('<html');
+        res.write('<header><title>Form Submit</title></header>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">send</button></form></body>');
+        res.write('</html');
         return res.end();
     }
     if(url == '/message' && method ==='POST'){
@@ -41,18 +41,18 @@ const taskHandler = (req,res)=>{
         res.write('<body><h3>welcome dear user</h3>')
         res.write('<form action="/create-user" method="POST"><input type="text" name="create-user"/><button type="submit">create</button></form>')
         res.write('</body>')
-       return res.end()
+       return res.end();
     }
     if(url==='/users' && method==='POST'){
         res.write('<html><body>')
-        res.write('<ul><li>Management</li><li>Miles</li><li>Jon</li><li>Doe</li></ul>')
+        res.write('<ul><li>Management</li><li>Miles</li><li>Jon</li><li>Doe</li></ul>');
         res.write('</body><html>')
-        return res.end()
+        return res.end();
     }
     if(url=== '/create-user'){
         const username = [];
         req.on('data',(chunk)=>{
-            username.push(chunk)
+            username.push(chunk);
             log(chunk)
         })
         return req.on('end',()=>{
