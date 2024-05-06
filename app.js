f0cd150bf0459bd59d8f8b668422d9cde460dce2
isaac-miles <isaacnice247@gmail.com>
2024-05-06 16:52:41 +0100

    .

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const {log} = console;
 
 let PORT = 4000;
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.use('/add-product', (req,res,next)=>{
     res.send('<body><form action="/product" method="POST"><input type="text" name="product"/><button type="submit">send</button></form></body>');
 })
-app.use('/product',(req,res,next)=>{
-    log(req.body);
+app.post('/product',(req,res,next)=>{
+    log(req.body.product);
     res.redirect('/');
 })
 app.use('/', (req,res,next)=>{
