2cf2a57958fc23b515b7c9fa06f22c50812ad9d3
isaac-miles <isaacnice247@gmail.com>
2024-05-06 17:42:18 +0100

    serving files

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const Router = require('express').Router();
 const {log} = console;
 
 Router.get('/add-product', (req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
 })
 
 Router.post('/product',(req,res,next)=>{
