14e40d6973bfa850bd385e120e17f2c6b126dd78
isaac-miles <isaacnice247@gmail.com>
2024-05-06 15:08:37 +0100

    .

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use('/', (req,res,next)=>{
 app.use('/add-products', (req,res,next)=>{
     log('in the second middleware')
     res.send('<body><form action="/products" method="POST"><input type="text" name="product"/><button type="submit">send</button></form></body>');
-
+})
+app.use('/product',(req,res,next)=>{
+    res.redirect('/')
 })
 app.listen(PORT,()=>`server running on port ${PORT}`);
