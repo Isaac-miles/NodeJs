21fc05714ce195b04e65f8c9eee6d459121215d6
isaac-miles <isaacnice247@gmail.com>
2024-05-06 17:00:31 +0100

    routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,13 @@
-const express = require('express').Router;
+const Router = require('express').Router();
 
-const app = express();
-app.use('/add-product', (req,res,next)=>{
+
+Router.get('/add-product', (req,res,next)=>{
     res.send('<body><form action="/product" method="POST"><input type="text" name="product"/><button type="submit">send</button></form></body>');
 })
-app.post('/product',(req,res,next)=>{
+
+Router.post('/product',(req,res,next)=>{
     log(req.body.product);
     res.redirect('/');
 })
+
+module.exports = Router;
