2399020f1d862c112dd76297434b3a68ee628bd9
isaac-miles <isaacnice247@gmail.com>
2024-05-06 14:30:08 +0100

    .

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const http = require('http');
 
 const express = require('express');
 const {log} = console;
@@ -7,9 +6,12 @@ let PORT = 4000;
 const app = express();
 
 app.use((req,res,next)=>{
-    log('in the first middleware')
+    log('in the first middleware');
+    next();
+})
+app.use((req,res,next)=>{
+    log('in the second middleware')
+    res.send('<h1>Hello</h1>')
 })
 
-const server = http.createServer(app);
-
-server.listen(PORT,()=>`server running on port ${PORT}`)
+app.listen(PORT,()=>`server running on port ${PORT}`)
