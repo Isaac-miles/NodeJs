1aea9e2d6cad5689d3ae808b19978fbdeefd8ef0
isaac-miles <isaacnice247@gmail.com>
2024-05-06 14:11:26 +0100

    .

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const express = require('express');
+const {log} = console;
 
+let PORT = 4000;
 const app = express();
 
+app.use((req,res,next)=>{
+    log('in the first middleware')
+})
+
 const server = http.createServer(app);
+
+server.listen(PORT,()=>`server running on port ${PORT}`)
