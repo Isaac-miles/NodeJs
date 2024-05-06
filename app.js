675f3aa1ec290b2fd6bda37c2152817f99e3d9c1
isaac-miles <isaacnice247@gmail.com>
2024-05-06 15:14:02 +0100

    body parser

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const {log} = console;
 let PORT = 4000;
 const app = express();
 
-app.use('/', (req,res,next)=>{
-    log('in the second middleware')
-    res.send('<h1>Hello</h1>');
-})
-app.use('/add-products', (req,res,next)=>{
-    log('in the second middleware')
-    res.send('<body><form action="/products" method="POST"><input type="text" name="product"/><button type="submit">send</button></form></body>');
+app.use('/add-product', (req,res,next)=>{
+    res.send('<body><form action="/product" method="POST"><input type="text" name="product"/><button type="submit">send</button></form></body>');
 })
 app.use('/product',(req,res,next)=>{
-    res.redirect('/')
+    log(req.body);
+    res.redirect('/');
 })
+app.use('/', (req,res,next)=>{
+    res.send('<h1>Hello</h1>');
+})
+
 app.listen(PORT,()=>`server running on port ${PORT}`);
