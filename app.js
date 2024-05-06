d30dd9405f058ad6f2a6cde686af73d1707b9488
isaac-miles <isaacnice247@gmail.com>
2024-05-06 18:37:30 +0100

    express  module done

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
+
 //you can add a filter to this base url
 app.use('/admin',adminRoutes);
 
