76254e8a05271143a02a5b6dbd8753ef191f6e4b
isaac-miles <isaacnice247@gmail.com>
2024-05-07 23:35:47 +0100

    .

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const bodyParser = require('body-parser');
 const express = require('express');
-const {engine} = require('express-handlebars');
+// const {engine} = require('express-handlebars');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -10,9 +10,15 @@ const shopRoutes = require('./routes/shop');
 let PORT = 4000;
 const app = express();
 
-app.engine('handlebars',engine());
-app.set('view engine', 'handlebars'); 
-app.set('views','./views/handlebars')
+//for ejs
+app.set('view engine', 'ejs');
+
+//for handlebars
+// app.engine('handlebars',engine());
+// app.set('view engine', 'handlebars'); 
+// app.set('views','./views/handlebars')
+
+//for pug
 // app.set('view engine', 'pug'); //set the configuration for the template engine
 // app.set('views','views') //telling express where to find dynamic templates for pug
 
