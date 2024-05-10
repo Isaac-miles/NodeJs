bd08c7f377d9ea4df29b5f32bc80adbaccf6eb57
isaac-miles <isaacnice247@gmail.com>
2024-05-10 23:38:32 +0100

    .

--- models/cart.js
+++ models/cart.js
@@ -19,11 +19,13 @@ export default class Cart{
         //Analyze the cart => find existing product
         const existingProductIndex = cart.products.findIndex(prod=>prod.id===id);
         const existingProduct = cart.products[existingProductIndex];
-        
+
             let updatedProduct;
             if(existingProduct){
                 updatedProduct={...existingProduct};
                 updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
             }else{
                 updatedProduct = {id:id,qty:1};
             cart.products = [...cart.products, updatedProduct]
