6e2ad66609a22a7b46acdeac2afe9b2ebf016b87
isaac-miles <isaacnice247@gmail.com>
2024-05-10 23:34:19 +0100

    .

--- models/cart.js
+++ models/cart.js
@@ -8,7 +8,7 @@ const p = path.join(
 
 export default class Cart{
 
-    static addProduct(id){
+    static addProduct(id,productPrice){
         //fetch the previous cart
         fs.readFile(p,(err,data)=>{
             let cart = {products:[],totalPrice:0};
@@ -22,7 +22,10 @@ export default class Cart{
             if(existingProduct){
                 updatedProduct={...existingProduct};
                 updatedProduct.qty = updatedProduct.qty + 1;
+            }else{
+                updatedProduct = {id:id,qty:1};
             }
+            cart.totalPrice = cart.totalPrice + productPrice;
         })
       
 
