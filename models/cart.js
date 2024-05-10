21585b01d4412d170afde48d7bec5b354fb8db60
isaac-miles <isaacnice247@gmail.com>
2024-05-10 22:17:54 +0100

    .

---
+++ models/cart.js
@@ -0,0 +1,11 @@
+
+module.exports = class Cart{
+    
+    static addProduct(id){
+        //fetch the previous cart
+
+        //Analyze the cart => find existing product
+
+        //Add new product increase quantity
+    }
+}
