4e0d8a178323e0f2a53e7d806a540ddc199ca0b4
isaac-miles <isaacnice247@gmail.com>
2024-05-13 20:48:04 +0100

    .

--- models/cart.js
+++ models/cart.js
@@ -40,9 +40,16 @@ module.exports = class Cart{
     }
     static deleteProduct(id,productPrice){
         fs.readFile(p,(err,data)=>{
+            let cart;
             if(err){
                 return
             }
+            cart = JSON.parse(data);
+            const updatedCart = {...cart};
+            const product = updatedCart.products.find(prod=>prod.id===id);
+            const productQty = product.qty;
+            updatedCart.products = updatedCart.products.filter(prod=>prod.id !== id);
+            updatedCart.totalPrice -= (productPrice * productQty);
         });
 
     }
