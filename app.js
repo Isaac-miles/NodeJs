6f18175f5f846eecc6d13b55321bf428235f2de5
isaac-miles <isaacnice247@gmail.com>
2024-05-16 01:14:14 +0100

    resolved host name

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 db.execute('SELECT * FROM products')
-    .then(data=>{
-
+    .then(result=>{
+        console.log(result);
     })
     .catch(err=>console.log(err));
 
--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host:'local',
+    host:'127.0.0.1',
     user:'root',
     database:'node-complete',
     password:'Isaacmiles247$'
