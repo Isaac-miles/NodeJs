b6f4d0890636ba0efa589f386f5d87099ed44604
isaac-miles <isaacnice247@gmail.com>
2024-05-19 23:32:20 +0100

    adding quantty

--- controllers/shop.js
+++ controllers/shop.js
@@ -70,6 +70,8 @@ exports.getCart = (req,res,next)=>{
 exports.addToCart = (req,res,next)=>{
     const productId = req.body.productId;
     let fetchedCart;
+    let newQuantity = 1;
+
     req.user.getCart()
         .then(cart=>{
             fetchedCart= cart;
@@ -80,18 +82,21 @@ exports.addToCart = (req,res,next)=>{
             if(products.length > 0){
                 product = products[0];
             }
-            let newQuantity = 1;
             if(product){
                 //get the quantity and change it
+                const oldQuantity = product.cartItem.quantity;
+                newQuantity = oldQuantity+1;
+                return product;
             }
             return ProductsModel.findByPk(productId)
-                .then(product=>{
-                    fetchedCart.addProduct(product,{through:{quantity:newQuantity}});
-                })
-                .catch(err=>console.log(err))
         })
+        .then(product=>{
+            return fetchedCart.addProduct(product,{
+                through:{quantity:newQuantity}
+            })
+        })
+        .then(()=>res.redirect('/cart'))
         .catch(err=>console.log(err))
-    // res.redirect('/cart');
 }
 exports.getOrders = (req,res,next)=>{
     res.render('shop/orders',
