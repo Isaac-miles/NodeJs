8738dcbc22392b062b8a9f9e4933442432b72dbb
isaac-miles <isaacnice247@gmail.com>
2024-05-19 21:27:01 +0100

    created cart model

--- models/cart.js
+++ models/cart.js
@@ -1,72 +1,15 @@
-const fs = require('fs');
-const path = require('path');
-const RootDIRE = require('../utils/path');
 
-const p = path.join(
-   RootDIRE,'data','cart.json'
-);
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../utils/db');
 
-module.exports = class Cart{
+const CartModel = sequelize.define('cart',{
+    id:{
+        type:DataTypes.STRING,
+        autoIncrement:true,
+        allowNull:false,
+        primaryKey:true
+    },
+    
+});
 
-    static addProduct(id,productPrice){
-        //fetch the previous cart
-        fs.readFile(p,(err,data)=>{
-            let cart = {products:[],totalPrice:0};
-            if(!err){
-                cart = JSON.parse(data);
-            }
-
-        //Analyze the cart => find existing product
-        const existingProductIndex = cart.products.findIndex(prod=>prod.id===id);
-        const existingProduct = cart.products[existingProductIndex];
-
-            let updatedProduct;
-            if(existingProduct){
-                updatedProduct={...existingProduct};
-                updatedProduct.qty = updatedProduct.qty + 1;
-                cart.products = [...cart.products];
-                cart.products[existingProductIndex] = updatedProduct;
-            }else{
-                updatedProduct = {id:id,qty:1};
-                cart.products = [...cart.products, updatedProduct]
-
-            }
-            cart.totalPrice = cart.totalPrice + +productPrice;
-            fs.writeFile(p,JSON.stringify(cart),err=>console.log(err));
-        });
-      
-
-        //Add new product increase quantity
-    }
-    static deleteProduct(id,productPrice){
-        fs.readFile(p,(err,data)=>{
-            let cart;
-            if(err){
-                return
-            }
-            cart = JSON.parse(data);
-            const updatedCart = {...cart};
-            const product = updatedCart.products.find(prod=>prod.id===id);
-            if(!product){
-                return;
-            }
-            const productQty = product.qty;
-            updatedCart.products = updatedCart.products.filter(prod=>prod.id !== id);
-            updatedCart.totalPrice -= (productPrice * productQty);
-            fs.writeFile(p,JSON.stringify(updatedCart),err=>console.log(err));
-
-        });
-
-    }
-
-    static getCart(cb){
-        fs.readFile(p,(err, data)=>{
-            const cart = JSON.parse(data);
-            if(err){
-                cb(null);
-            }else{
-                cb(cart);
-            }
-        })
-    }
-}
+module.exports = CartModel;
