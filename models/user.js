1d20de2527e5b4d4c9f448b6e47f2e573dab0c4d
isaac-miles <isaacnice247@gmail.com>
2024-05-19 20:10:32 +0100

    .

--- models/user.js
+++ models/user.js
@@ -1,3 +1,11 @@
 const {Sequelize, DataTypes} = require('sequelize');
 
-const UserModel = new Sequelize({})
+const UserModel = new Sequelize.define('user',{
+    id:{
+        type:DataTypes.INTEGER,
+        autoIncrement:true,
+        allowNull:false,
+        primaryKey:true
+
+    }
+})
