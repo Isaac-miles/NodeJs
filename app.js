ce05a6590b94811fcdc9bacaa5bfea98bb45598f
isaac-miles <isaacnice247@gmail.com>
2024-05-19 20:50:23 +0100

    create a new user if it doesn't exists

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const shopRoutes = require('./routes/shop');
 const sequelize = require('./utils/db');
 const ProductsModel = require('./models/product');
 const UserModel = require('./models/user');
+const { name } = require('ejs');
 
 let PORT = 4000;
 const app = express();
@@ -35,17 +36,31 @@ app.use(errorHandlerController.get404);
  async function connectToDataBase(){
     ProductsModel.belongsTo(UserModel,{constraints:true, onDelete:'CASCADE' });
     UserModel.hasMany(ProductsModel);
-    
+
     try {
 
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
+        // sequelize.sync({force:true}) // on load the app drops existing tables and create a new one
         sequelize.sync()
           .then(result=>{
-            // console.log(result);
+            return UserModel.findByPk(1);
+          })
+          .then(user=>{
+            if(!user){
+               return UserModel.create({
+                name:"Miles",
+                email:"miles@test.com"
+            });
+            }
+            return user;
+          })
+          .then(user=>{
+            // console.log(user);
             app.listen(PORT,()=>`server running on port ${PORT}`);
 
-          }).catch(err=>console.log(err));
+          })
+          .catch(err=>console.log(err));
 
       } catch (error) {
         console.error('Unable to connect to the database:', error);
--- models/user.js
+++ models/user.js
@@ -1,15 +1,21 @@
-const {Sequelize, DataTypes} = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../utils/db');
 
-const UserModel = new sequelize.define('user',{
+const UserModel =  sequelize.define('user',{
     id:{
         type:DataTypes.INTEGER,
         autoIncrement:true,
         allowNull:false,
         primaryKey:true
     },
-    name:DataTypes.STRING,
-    email:DataTypes.STRING
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    email:{
+        type:DataTypes.STRING,
+        allowNull:false
+    }
 })
 
 module.exports = UserModel;
