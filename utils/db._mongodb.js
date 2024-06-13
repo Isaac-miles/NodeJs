eb1e498e68a01b6d0e9a2ee1c5d95c833608677b
isaac-miles <isaacnice247@gmail.com>
2024-06-13 15:55:44 +0100

    setting connections

--- utils/db._mongodb.js
+++ utils/db._mongodb.js
@@ -1,8 +1,12 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect("mongodb+srv://exploreNode:exploreNode @cluster0.hp6xb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-        .then(result=>{
-            console.log("Connection Established");
-        })
-        .catch(err=>console.log(err));
+const connectToMongodb =(callback)=>{
+    MongoClient.connect("mongodb+srv://exploreNode:exploreNode @cluster0.hp6xb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    .then(result=>{
+        callback("Connection Established");
+    })
+    .catch(err=>console.log(err));
+}
+
+module.exports = connectToMongodb;
